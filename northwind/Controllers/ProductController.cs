5534689ebbc178e227871acffdbaf045a49cf4ea
Nick Saeger <nsaeger1@my.wctc.edu>
2021-03-02 11:58:04 -0600

    Initial commit

---
+++ northwind/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Northwind.Models;
+
+namespace Northwind.Controllers
+{
+    public class ProductController : Controller
+    {
+        // this controller depends on the NorthwindRepository
+        private INorthwindRepository repository;
+        public ProductController(INorthwindRepository repo) => repository = repo;
+
+        public IActionResult Category() => View(repository.Categories.OrderBy(c => c.CategoryName));
+        public IActionResult Index(int id) => View(repository.Products.Where(p => p.CategoryId == id && p.Discontinued == false).OrderBy(p => p.ProductName));
+    }
+}
